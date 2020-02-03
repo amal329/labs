ec739eaeccfb4bf0f6415e5c037b6700ee197683
amal329 <32464420+amal329@users.noreply.github.com>
2020-02-03 12:23:14 +0530

    Add files via upload

---
+++ hypercube.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool differbyone(int a, int b){
+	int x=a^b;
+	if(x && (!(x & (x-1))))
+		return true;
+	else
+		return false;	
+}
+
+void print(vector<int> g[],int m)
+{   
+	cout<<"The adjacency list representation of the graph is:"<<endl;
+    for(int i=0;i<m;i++)
+    {	
+	int degree=0;
+        cout<<i<<": ";
+        for(int j=0;j<g[i].size();j++)
+        {
+        	cout<<" -> ";
+			degree++;
+            cout<<g[i][j]<<" ";
+        }
+        cout<<endl;
+	cout<<"Degree : "<<degree<<endl;
+    }
+}
+
+int main(){
+	
+	int deg,size;
+	cout<<"Enter the degree: "<<endl;
+	cin>>deg;
+	
+	size=pow(2,deg);
+	cout<<size<<endl;
+	vector<int> g[size];
+	set<int> s;
+	
+	for(int i=0;i<size;i++){
+		s.insert(i);
+	}
+	
+	for(int i=0;i<size;i++){
+		int n1=i;
+		set<int> ::iterator itr;
+		for(itr=s.begin();itr!=s.end();itr++){
+			if(differbyone(n1,*(itr))){
+				g[n1].push_back(*itr);
+			}
+		}
+	}
+	print(g,size);
+}
